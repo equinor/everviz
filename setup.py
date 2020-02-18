16ccc90f5675a3e7854b669d4dc7629cd0637a96
Lars Petter Øren Hauge <lpha@equinor.com>
2020-02-18 12:53:28 +0100

    Add travis.yml

--- setup.py
+++ setup.py
@@ -10,12 +10,9 @@ setup(
     author="Equinor ASA",
     url="https://github.com/equinor/everviz",
     setup_requires=["pytest-runner", "setuptools_scm"],
+    install_requires=["pyyaml"],
     test_suite="tests",
     use_scm_version={"write_to": "everviz/version.py"},
-    tests_require=[
-        "pytest",
-        'black',
-    ],
     classifiers=[
         "Programming language :: Python",
         "Programming language :: Python :: 3.6",
