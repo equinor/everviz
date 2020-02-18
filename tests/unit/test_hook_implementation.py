f7fc7310a1fc4e3f4b440636fd20da9ccca41fd5
Lars Petter Øren Hauge <lpha@equinor.com>
2020-02-18 13:04:30 +0100

    Black repo

--- tests/unit/test_hook_implementation.py
+++ tests/unit/test_hook_implementation.py
@@ -1,7 +1,7 @@
-
 import pytest
 
 from everviz import everest_hooks
+
 
 def test_everest_hooks_registered():
     hook_manager = pytest.importorskip("everest.plugins.hook_manager")
@@ -10,4 +10,4 @@ def test_everest_hooks_registered():
     pm.register(everest_hooks)
 
     last_registered = pm.hook.visualize_data.get_hookimpls()[-1]
-    assert last_registered.plugin_name == 'everviz.everest_hooks'
+    assert last_registered.plugin_name == "everviz.everest_hooks"
