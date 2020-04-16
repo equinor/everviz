1d0c54cc7eafb7e7a9480eb7cd2cb29a36523838
Peter Verveer <pieter.verveer@tno.nl>
2020-04-16 09:40:09 +0200

    Multiple objective plot

--- everviz/plugins/objectives_plot/objectives_plot.py
+++ everviz/plugins/objectives_plot/objectives_plot.py
@@ -1,11 +1,14 @@
 from uuid import uuid4
 from pathlib import Path
+from itertools import cycle
 import pkg_resources
 
 import dash_html_components as html
 import dash_core_components as dcc
 
 import plotly.graph_objs as go
+from plotly.colors import DEFAULT_PLOTLY_COLORS
+
 from dash.dependencies import Output, Input
 from webviz_config import WebvizPluginABC
 from webviz_config.webviz_assets import WEBVIZ_ASSETS
@@ -13,56 +16,68 @@ from everviz.data.load_csv.get_data import get_data
 
 
 class ObjectivesPlot(WebvizPluginABC):
-    def __init__(
-        self,
-        app,
-        data_path,
-        x_title="batch",
-        y_title="value",
-        title="Objective function values",
-    ):
+    def __init__(self, app, values_file, statistics_file):
         super().__init__()
-        self.title = title
-        self.x_title = x_title
-        self.y_title = y_title
-        self.graph_id = f"graph-{uuid4()}"
-        self.filter_box_id = f"dropdown-{uuid4()}"
-        self.radio_id = f"dropdown-{uuid4()}"
 
-        self.data_path = data_path
+        self.graph_id = f"graph-{uuid4()}"
+        self.radio_id = f"dropdown-{uuid4()}"
+        self.function_dropdown_id = f"dropdown-{uuid4()}"
+
+        self.values_file = values_file
+        self.statistics_file = statistics_file
         self.set_callbacks(app)
 
         ASSETS_DIR = Path(pkg_resources.resource_filename("everviz", "assets"))
-
         WEBVIZ_ASSETS.add(ASSETS_DIR / "axis_customization.css")
 
     def add_webvizstore(self):
-        return [(get_data, [{"data_path": self.data_path}])]
+        return [
+            (get_data, [{"values_file": self.values_file}]),
+            (get_data, [{"statistics_file": self.statistics_file}]),
+        ]
 
     @property
     def layout(self):
+        data = get_data(self.statistics_file)
+
+        functions = data["function"].unique()
+        function_dropdown_options = [{"label": i, "value": i} for i in functions]
+        func_elements = [
+            html.Label("Functions to plot:"),
+            dcc.Dropdown(
+                id=self.function_dropdown_id,
+                options=function_dropdown_options,
+                multi=True,
+                value=[functions[0]],
+            ),
+        ]
+
         radio_options = ["Statistics", "Data"]
+        radio_elements = [
+            dcc.RadioItems(
+                id=self.radio_id,
+                options=[{"label": i, "value": i} for i in radio_options],
+                value=radio_options[0],
+                labelStyle={"display": "inline-block"},
+                style={"margin-top": 20},
+            )
+        ]
+
         return html.Div(
             [
-                html.H1(children=self.title, style={"textAlign": "center"}),
                 html.Div(
                     [
                         html.Div(
-                            [dcc.Graph(id=self.graph_id)],
-                            style={"width": "99%", "display": "inline-block"},
+                            func_elements + radio_elements,
+                            style={
+                                "width": "29%",
+                                "display": "inline-block",
+                                "vertical-align": "top",
+                            },
                         ),
                         html.Div(
-                            [
-                                dcc.RadioItems(
-                                    id=self.radio_id,
-                                    options=[
-                                        {"label": i, "value": i} for i in radio_options
-                                    ],
-                                    value=radio_options[0],
-                                    labelStyle={"display": "inline-block"},
-                                ),
-                            ],
-                            style={"textAlign": "center"},
+                            [dcc.Graph(id=self.graph_id)],
+                            style={"width": "69%", "display": "inline-block"},
                         ),
                     ]
                 ),
@@ -71,38 +86,77 @@ class ObjectivesPlot(WebvizPluginABC):
 
     def set_callbacks(self, app):
         @app.callback(
-            Output(self.graph_id, "figure"), [Input(self.radio_id, "value"),],
+            Output(self.graph_id, "figure"),
+            [Input(self.function_dropdown_id, "value"), Input(self.radio_id, "value"),],
         )
-        def update_graph(graph_type):
-            df = get_data(self.data_path)
+        def update_graph(func_list, radio_value):
+            # # The key_list arguments is the list of functions to plot.
+            if func_list is None:
+                return {}
+
+            # # Get the data, setting its index.
+            if radio_value == "Statistics":
+                data = get_data(self.statistics_file).set_index("function")
+            else:
+                data = get_data(self.values_file).set_index("function")
+
+            # Make a cycle iterator over the plotly colors.
+            colors = cycle(DEFAULT_PLOTLY_COLORS)
+
             traces = []
-            if graph_type == "Statistics":
-                for label in ["P10", "Mean", "P90"]:
-                    fill = "tonexty" if label in ["P90", "Mean"] else None
+            for color, key in zip(colors, func_list):
+                # Select all data belonging to the current key.
+                func_data = data.xs(key)
+
+                # Make the traces, with mean, P10 and P90, shading in between.
+                if radio_value == "Statistics":
+                    traces.extend(
+                        [
+                            go.Scatter(
+                                y=func_data["P90"],
+                                x=func_data["batch"],
+                                mode="lines",
+                                marker={"size": 10},
+                                line={"color": color},
+                                name=key + "(P90)",
+                                showlegend=False,
+                            ),
+                            go.Scatter(
+                                y=func_data["P10"],
+                                x=func_data["batch"],
+                                mode="lines",
+                                line={"color": color},
+                                marker={"size": 10},
+                                fill="tonexty",
+                                name=key + "(P10)",
+                                showlegend=False,
+                            ),
+                            go.Scatter(
+                                y=func_data["Mean"],
+                                x=func_data["batch"],
+                                mode="lines",
+                                marker={"size": 10},
+                                line={"color": color},
+                                name=key,
+                                showlegend=True,
+                            ),
+                        ]
+                    )
+                else:
                     traces.append(
                         go.Scatter(
-                            y=df[label],
-                            x=df[self.x_title],
-                            name=label,
-                            mode="lines",
+                            y=func_data["value"],
+                            x=func_data["batch"],
+                            mode="markers",
                             marker={"size": 10},
-                            fill=fill,
-                            fillcolor="rgba(0,40,100,0.2)",
+                            name=key,
+                            showlegend=True,
                         )
                     )
-            else:
-                traces.append(
-                    go.Scatter(
-                        y=df[self.y_title],
-                        x=df[self.x_title],
-                        mode="markers",
-                        marker={"size": 10},
-                    )
-                )
+
             return {
                 "data": traces,
                 "layout": dict(
-                    xaxis={"title": self.x_title.capitalize()},
-                    yaxis={"title": self.y_title.capitalize()},
+                    xaxis={"title": "Batch"}, yaxis={"title": "Function Key Value"},
                 ),
             }
--- tests/unit/test_objective_plot_data.py
+++ tests/unit/test_objective_plot_data.py
@@ -1,37 +1,99 @@
 import os
 import pandas as pd
+import numpy as np
 import pytest
 
 import everviz
 
-from everviz.pages.objectives import _calc_p10_p90, _set_up_data_sources, _calc_mean
+from everviz.pages.objectives import (
+    _set_up_data_sources,
+    _objective_values,
+    _objective_statistics,
+    _total_objective_values,
+)
 
 OBJECTIVES = [
-    {"batch": 0, "realization": 1, "function": "f0", "value": 100, "simulation": None},
-    {"batch": 0, "realization": 1, "function": "f1", "value": 200, "simulation": None},
-    {"batch": 2, "realization": 2, "function": "f0", "value": 200, "simulation": None},
-    {"batch": 2, "realization": 2, "function": "f1", "value": 400, "simulation": None},
-    {"batch": 0, "realization": 3, "function": "f0", "value": 300, "simulation": None},
-    {"batch": 0, "realization": 3, "function": "f1", "value": 600, "simulation": None},
-    {"batch": 2, "realization": 4, "function": "f0", "value": 400, "simulation": None},
-    {"batch": 2, "realization": 4, "function": "f1", "value": 800, "simulation": None},
+    {"batch": 0, "realization": 1, "function": "f0", "value": 100, "simulation": 0},
+    {"batch": 0, "realization": 1, "function": "f1", "value": 200, "simulation": 1},
+    {"batch": 2, "realization": 2, "function": "f0", "value": 200, "simulation": 2},
+    {"batch": 2, "realization": 2, "function": "f1", "value": 400, "simulation": 3},
+    {"batch": 0, "realization": 3, "function": "f0", "value": 300, "simulation": 4},
+    {"batch": 0, "realization": 3, "function": "f1", "value": 600, "simulation": 5},
+    {"batch": 2, "realization": 4, "function": "f0", "value": 400, "simulation": 6},
+    {"batch": 2, "realization": 4, "function": "f1", "value": 800, "simulation": 7},
 ]
+
+
+def test_objective_values_data_frame():
+    """Test for the correct layout and size of the objective values data frame."""
+    objective_values = _objective_values(pd.DataFrame(OBJECTIVES))
+
+    assert set(objective_values.columns) == {
+        "batch",
+        "function",
+        "value",
+        "simulation",
+    }
+    assert len(objective_values) == len(OBJECTIVES)
+    assert set(objective_values["batch"]) == {obj["batch"] for obj in OBJECTIVES}
+    assert set(objective_values["function"]) == {obj["function"] for obj in OBJECTIVES}
+    assert set(objective_values["simulation"]) == {
+        obj["simulation"] for obj in OBJECTIVES
+    }
+
+
+def test_objective_statistics_data_frame():
+    """Test for the correct layout and size of the objective statistics data frame"""
+    objective_statistics = _objective_statistics(pd.DataFrame(OBJECTIVES))
+
+    assert set(objective_statistics.columns) == {
+        "batch",
+        "function",
+        "Mean",
+        "P10",
+        "P90",
+    }
+    assert len(objective_statistics) == 2 * 2  # #batches * #functions
+    assert set(objective_statistics["batch"]) == {obj["batch"] for obj in OBJECTIVES}
+    assert set(objective_statistics["function"]) == {
+        obj["function"] for obj in OBJECTIVES
+    }
+
+
+def test_objective_statistics_content():
+    """Test for the correct content of the objectives statistics data frame."""
+    objective_statistics = _objective_statistics(pd.DataFrame(OBJECTIVES))
+
+    mean = []
+    p10 = []
+    p90 = []
+    for func in ["f0", "f1"]:
+        for batch in [0, 2]:
+            values = [
+                obj["value"]
+                for obj in OBJECTIVES
+                if obj["batch"] == batch and obj["function"] == func
+            ]
+            mean.append(np.mean(values))
+            p10.append(np.quantile(values, q=0.1))
+            p90.append(np.quantile(values, q=0.9))
+
+    assert objective_statistics["Mean"].to_list() == mean
+    assert objective_statistics["P10"].to_list() == p10
+    assert objective_statistics["P90"].to_list() == p90
 
 
 def test_insert_p10_p90():
     df = pd.DataFrame(OBJECTIVES)
-    result = _calc_p10_p90(df)
+    result = _objective_statistics(df)
     for header in ["P10", "P90"]:
         assert header in result
 
 
 def test_mean():
     df = pd.DataFrame(OBJECTIVES)
-    result = _calc_mean(df)
-    assert all(
-        result["Mean"].values
-        == [300.0, 300.0, 450.0, 450.0, 300.0, 300.0, 450.0, 450.0]
-    )
+    result = _objective_statistics(df)
+    assert all(result["Mean"].values == [200.0, 300.0, 400.0, 600.0])
 
 
 @pytest.mark.parametrize(
@@ -39,7 +101,7 @@ def test_mean():
 )
 def test_p10_p90(header_1, header_2):
     df = pd.DataFrame(OBJECTIVES)
-    result = _calc_p10_p90(df)
+    result = _objective_statistics(df)
     for val_1, val_2 in zip(result[header_1], result[header_2]):
         assert val_1 < val_2
 
@@ -50,8 +112,34 @@ def test_set_up_sources(mocker, monkeypatch, tmpdir):
     os.mkdir(os.path.join(tmpdir, "everviz"))
     monkeypatch.setattr(
         everviz.pages.objectives,
-        "_objective_values",
+        "_objective_values_from_api",
         mocker.Mock(return_value=pd.DataFrame(OBJECTIVES)),
+    )
+    total_data = pd.DataFrame(OBJECTIVES).drop(
+        columns=["realization", "function", "simulation"]
+    )
+    monkeypatch.setattr(
+        everviz.pages.objectives,
+        "_total_objective_values_from_api",
+        mocker.Mock(return_value=total_data),
     )
     data_source = _set_up_data_sources(mock_api)
     assert data_source.objective_values == tmpdir / "everviz" / "objective_values.csv"
+    assert (
+        data_source.objective_statistics
+        == tmpdir / "everviz" / "objective_statistics.csv"
+    )
+    assert (
+        data_source.total_objective_values
+        == tmpdir / "everviz" / "total_objective_values.csv"
+    )
+
+
+def test_total_objective_values_data_frame():
+    total_data = pd.DataFrame(OBJECTIVES).drop(
+        columns=["realization", "function", "simulation"]
+    )
+    total_objective_values = _total_objective_values(total_data)
+
+    assert set(total_objective_values.columns) == {"batch", "value"}
+    assert len(total_objective_values) == len(OBJECTIVES)
