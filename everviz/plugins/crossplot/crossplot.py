6665e84451d293ba7ce7d89abce642da31168a22
DanSava <dan.sava42@gmail.com>
2020-05-29 13:19:09 +0300

    test pr

--- everviz/plugins/crossplot/crossplot.py
+++ everviz/plugins/crossplot/crossplot.py
@@ -89,7 +89,12 @@ class Crossplot(WebvizPluginABC):
                             },
                         ),
                         html.Div(
-                            [dcc.Graph(id=self.graph_id, config={"modeBarButtonsToRemove":["toImage"]})],
+                            [
+                                dcc.Graph(
+                                    id=self.graph_id,
+                                    config={"modeBarButtonsToRemove": ["toImage"]},
+                                )
+                            ],
                             style={"width": "69%", "display": "inline-block"},
                         ),
                     ]
