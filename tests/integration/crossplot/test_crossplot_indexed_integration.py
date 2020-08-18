b2cb7b3288a1606e452ceaf23af94c18166e88d0
DanSava <dan.sava42@gmail.com>
2020-08-18 13:38:47 +0300

    Update test_crossplot_indexed_integration

--- tests/integration/crossplot/test_crossplot_indexed_integration.py
+++ tests/integration/crossplot/test_crossplot_indexed_integration.py
@@ -4,7 +4,11 @@ from everviz.plugins.crossplot.crossplot_indexed import CrossplotIndexed
 
 
 def test_crossplot_indexed_callback(app, dash_duo, monkeypatch, mocker, caplog):
-    mock_data = pd.DataFrame(data=[[1, 2, 3]], columns=["a", "b", "c"])
+    columns_names = ["a", "b", "c"]
+    mock_data = pd.DataFrame(
+        data=[[1, 2, 3]],
+        columns=[f"{name}-{idx}" for idx, name in enumerate(columns_names)],
+    )
     monkeypatch.setattr(
         everviz.plugins.crossplot.crossplot_indexed,
         "get_data",
@@ -15,7 +19,21 @@ def test_crossplot_indexed_callback(app, dash_duo, monkeypatch, mocker, caplog):
     app.layout = plugin.layout
     dash_duo.start_server(app)
 
+    dropdown_x = dash_duo.find_element(f"#{plugin.dropdown_x_id}")
+    dropdown_x.click()
+    options_x = dropdown_x.find_element_by_css_selector("div.Select-menu-outer")
+    assert columns_names == options_x.text.split("\n")
     dash_duo.clear_input("#{}".format(plugin.dropdown_x_id))
+
+    dash_duo.find_element(f"#{plugin.graph_id}").click()
+
+    dropdown_y = dash_duo.find_element(f"#{plugin.dropdown_y_id}")
+    dropdown_y.click()
+    options_y = dropdown_y.find_element_by_css_selector("div.Select-menu-outer")
+    assert columns_names == options_y.text.split("\n")
+    dash_duo.clear_input("#{}".format(plugin.dropdown_y_id))
+
+    dash_duo.clear_input("#{}".format(plugin.dropdown_realization_id))
 
     for record in caplog.records:
         assert record.levelname != "ERROR"
