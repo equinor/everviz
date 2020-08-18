90fd30b5e909eac03ac56e7f46732fc4b65bff9c
DanSava <dan.sava42@gmail.com>
2020-08-18 15:36:28 +0300

    Handle Ctrl-C in order to show exit message when running everviz.

--- everviz/everest_hooks.py
+++ everviz/everest_hooks.py
@@ -1,5 +1,9 @@
 import subprocess
 import shutil
+import signal
+import sys
+import os
+
 from everviz.util import get_everviz_folder
 from everviz.config import setup_default_everviz_config
 from everviz.log import setup_logger
@@ -17,10 +21,20 @@ except ImportError:
     hookimpl = mock_impl
 
 
+def handle_exit(*args):  # pylint: disable=unused-argument)
+    print("\n" + "=" * 32)
+    print("Session terminated by the user.\n" "Thank you for using Everviz!")
+    print("=" * 32)
+    sys.tracebacklimit = 0
+    sys.stdout = open(os.devnull, "w")
+    sys.exit()
+
+
 @hookimpl
 def visualize_data(api):
     everviz_folder = get_everviz_folder(api)
     logger = setup_logger(everviz_folder)
+    signal.signal(signal.SIGINT, handle_exit)
 
     config_path = setup_default_everviz_config(api)
     # The entry point of webviz is to call it from command line, and so do we.
